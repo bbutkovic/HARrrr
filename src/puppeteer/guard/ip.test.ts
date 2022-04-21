aad18beffd662375c4a6e85dba140308ef1d3780
Borna Butkovic <borna@favicode.net>
2022-04-21 13:46:46 +0200

    swap out IP parsing library

--- src/puppeteer/guard/ip.test.ts
+++ src/puppeteer/guard/ip.test.ts
@@ -1,4 +1,4 @@
-import { isIp, default as isIpAllowed } from "./ip";
+import { isIp, default as isIpAllowed, isPrivate } from "./ip";
 
 describe('is IP', () => {
     test('properly distinguish domains from IP addresses', () => {
@@ -22,9 +22,8 @@ describe('is IP allowed', () => {
         expect(isIpAllowed('169.254.169.254', [], true)).toBeFalsy();
         expect(isIpAllowed('::1', [], true)).toBeFalsy();
 
-        // TODO!
-        // expect(isIpAllowed('0000:0000:0000:0000:0000:0000:0000:0001', [], true)).toBeFalsy();
-        // expect(isIpAllowed('::0:1', [], true)).toBeFalsy();
+        expect(isIpAllowed('0000:0000:0000:0000:0000:0000:0000:0001', [], true)).toBeFalsy();
+        expect(isIpAllowed('::0:1', [], true)).toBeFalsy();
     });
 
     test('allow or disallow IPs from a list', () => {
@@ -32,5 +31,31 @@ describe('is IP allowed', () => {
         expect(isIpAllowed('127.0.0.1', ['127.0.0.1'], false)).toBeFalsy();
         expect(isIpAllowed('192.168.1.1', ['127.0.0.1'], false)).toBeTruthy();
         expect(isIpAllowed('::1', ['::1'], false)).toBeFalsy();
+    });
+
+    test('block a range of IP addresses', () => {
+        expect(isIpAllowed('192.168.1.5', [], false)).toBeTruthy();
+        expect(isIpAllowed('192.168.1.5', ['192.168.0.0/16'], false)).toBeFalsy();
+    });
+});
+
+describe('is IP private', () => {
+    test('ipv4 ranges are properly distinguished', () => {
+        expect(isPrivate('127.0.0.1')).toBeTruthy();
+        expect(isPrivate('127.127.127.127')).toBeTruthy();
+        expect(isPrivate('169.254.169.254')).toBeTruthy();
+        expect(isPrivate('8.8.8.8')).toBeFalsy();
+    });
+
+    test('ipv6 ranges are properly distinguished', () => {
+        expect(isPrivate('::1')).toBeTruthy();
+        expect(isPrivate('2001:4860:4860::8888')).toBeFalsy();
+        expect(isPrivate('::0:1')).toBeTruthy();
+        expect(isPrivate('0000:0000:0000:0000:0000:0000:0000:0001')).toBeTruthy();
+        expect(isPrivate('0:0:0:0:0:0:0:0001')).toBeTruthy();
+        expect(isPrivate('0:0:0:0:0:0:0:1')).toBeTruthy();
+        expect(isPrivate('0::0:0:0:1')).toBeTruthy();
+        expect(isPrivate('::0:0:0:1')).toBeTruthy();
+        expect(isPrivate('fd00:ec2::254')).toBeTruthy();
     });
 });
--- src/puppeteer/guard/ip.ts
+++ src/puppeteer/guard/ip.ts
@@ -1,9 +1,27 @@
-import { isPrivate, isV4Format, isV6Format } from "ip";
+import { IPv4, IPv6, isValid, parse, parseCIDR } from "ipaddr.js";
 
 export function isIp(host: string): boolean {
-    return isV4Format(host) || isV6Format(host);
+    return isValid(host);
 }
 
 export default function isIpAllowed(ip: string, blockIps: string[], blockPrivate: boolean): boolean {
-    return !(blockPrivate && isPrivate(ip)) && !blockIps.includes(ip);
+    const parsedIp = parse(ip);
+    return !(blockPrivate && isPrivate(parsedIp)) && !blockIps.some((blockedIp) => {
+        return blockedIp.includes('/') ?
+            parsedIp.match(parseCIDR(blockedIp)) :
+            parse(blockedIp).toNormalizedString() === parsedIp.toNormalizedString();
+    });
+}
+
+export function isPrivate(ip: string | IPv4 | IPv6): boolean {
+    if (typeof ip === 'string') {
+        ip = parse(ip);
+    }
+
+    if (ip.kind() === "ipv6" && (ip as IPv6).isIPv4MappedAddress()) {
+        ip = (ip as IPv6).toIPv4Address();
+    }
+
+    const range = ip.range();
+    return range === "private" || range === "linkLocal" || range === "loopback" || range === "uniqueLocal";
 }
