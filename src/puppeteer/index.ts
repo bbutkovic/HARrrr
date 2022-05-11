02e9b05dc69c359a511360af68e34070668910e1
Borna Butkovic <borna@favicode.net>
2022-05-11 13:12:32 +0200

    use fork of puppeteer-har

--- src/puppeteer/index.ts
+++ src/puppeteer/index.ts
@@ -1,4 +1,4 @@
-import PuppeteerHar from "puppeteer-har";
+import { captureNetwork } from "@kaytwo/puppeteer-har";
 import getBrowserInstance, { BrowserOptions } from "./browser";
 import setupGuard, { GuardOpts, isBlockedAccessError } from "./guard";
 
@@ -28,9 +28,8 @@ export default async function gotoAndCapture(url: string,
         setupGuard(page, guard, (status) => requestIsValid = status);
     }
 
-    const har = new PuppeteerHar(page);
-    await har.start({
-        saveResponse: true,
+    const getHar = await captureNetwork(page, {
+        saveResponses: true
     });
 
     try {
@@ -38,7 +37,7 @@ export default async function gotoAndCapture(url: string,
             waitUntil: "domcontentloaded"
         });
 
-        const result = await har.stop() as object;
+        const result = await getHar() as object;
         if (!requestIsValid) {
             console.info(`Invalid request to ${url}`);
             return null;
@@ -53,7 +52,6 @@ export default async function gotoAndCapture(url: string,
 
         throw e;
     } finally {
-        har.cleanUp();
         await page.close();
         await browser.close();
     }
